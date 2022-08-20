18aa10d09cdfa081f9148d883264841de28f8de0
Brandon Cozart <brandon.cozart2000@gmail.com>
2022-08-20 17:08:57 -0400

    Working With ML text Recognition

--- app/src/main/java/com/example/iftaproject/SecondFragment.java
+++ app/src/main/java/com/example/iftaproject/SecondFragment.java
@@ -4,22 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.iftaproject.databinding.FragmentSecondBinding;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
 
+
+
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        InputImage image;
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -28,6 +37,13 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonExtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
