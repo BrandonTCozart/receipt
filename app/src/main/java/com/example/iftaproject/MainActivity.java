8b1de61aa66ff4bd54022c8da6e8cf90f2f1c142
Brandon Cozart <brandon.cozart2000@gmail.com>
2022-08-20 03:04:21 -0400

    currentQuarter Fragment
    currentQuarter Fragment added and some other buttons on the secondFragment.

--- app/src/main/java/com/example/iftaproject/MainActivity.java
+++ app/src/main/java/com/example/iftaproject/MainActivity.java
@@ -19,7 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -37,13 +37,6 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/example/iftaproject/SecondFragment.java
+++ app/src/main/java/com/example/iftaproject/SecondFragment.java
@@ -36,6 +36,14 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        binding.buttonCurrentQuarter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_currentQuarterFragment);
+            }
+        });
     }
 
     @Override
