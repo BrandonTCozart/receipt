33b873f5df055c99c85c888f5de215c298fca2b0
Brandon Cozart <brandon.cozart2000@gmail.com>
2022-08-22 01:59:11 -0400

    Progress
    This version can get an image from the gallery and extract the text from it.

--- app/src/main/java/com/example/iftaproject/SecondFragment.java
+++ app/src/main/java/com/example/iftaproject/SecondFragment.java
@@ -1,25 +1,46 @@
 package com.example.iftaproject;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.iftaproject.databinding.FragmentSecondBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
+import java.io.IOException;
+
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+
+    String uriString = "";
+    TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
 
 
 
@@ -27,8 +48,8 @@ public class SecondFragment extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
-        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-        InputImage image;
+        //TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -38,10 +59,35 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.CAMERA
+            },
+                    100);
+
+        }
+
+        binding.ReceiptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+            }
+        });
+
+        binding.Tphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100); //Check
+
+            }
+        });
+
         binding.buttonExtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Uri uri = Uri.parse(uriString); // This captures the string of the image
+                _extractTextFromUri(uri); // Uses method defined below to extract data using the ML KIT.
             }
         });
 
@@ -60,7 +106,54 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_currentQuarterFragment);
             }
         });
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 100){
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            binding.imageViewReceipt.setImageBitmap(captureImage);
+            //binding.imageViewReceipt.setImageURI(data.getData());
+            //uriString = data.getData().toString();
+        }
+    }
+
+    public void _extractTextFromUri(Uri _uri){
+        try{
+            InputImage image = InputImage.fromFilePath(getContext(), _uri);
+            //InputImage image = InputImage.fromBitmap(captureImage, 0);
+            Task<Text> result =
+                    recognizer.process(image)
+                            .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                @Override
+                                public void onSuccess(Text visionText) {
+                                    binding.textViewTextResults.setText(visionText.getText());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+
+                                        }
+                                    });
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    binding.imageViewReceipt.setImageURI(result);
+                    uriString = result.toString();
+                }
+            });
 
     @Override
     public void onDestroyView() {
